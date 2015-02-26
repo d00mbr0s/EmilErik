65a0cec6195c3e64c7c5ea53238ce754783973b7
Emil <emilsandgren94@hotmail.com>
2015-02-26 15:10:07 +0100

    HEJ

--- Ny mapp/src/testGit/Wakalala.java
+++ Ny mapp/src/testGit/Wakalala.java
@@ -1,8 +1,10 @@
 package testGit;
 
+import javax.swing.JOptionPane;
+
 public class Wakalala {
 	public static void main(String[] args) {
-		System.out.println("då");
+		JOptionPane.showMessageDialog(null, "EYO");
 		
 	}
 
